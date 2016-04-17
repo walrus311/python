974fe66e1d97371e3af7be3e765761c6b77e23ba
Eric Wall <eric@WalrusMini.local>
2016-04-17 02:25:11 -0400

    Broke out password into separate file.

--- pushPhotos/pushPhotos.py
+++ pushPhotos/pushPhotos.py
@@ -14,9 +14,23 @@ from datetime import datetime
 logging.basicConfig(level=logging.INFO,   #Default to INFO
                     format='%(asctime)s %(name)-5s %(levelname)-8s %(message)s',
                     datefmt='%m-%d %H:%M',
-                    filename='/Users/eric/code/python/pushPhotos/pushPhotos.log',
+                    filename='/Users/eric/bin/pushPhotos.log',
                     filemode='w')
 log = logging.getLogger()
+
+#
+# Function to load password from a file
+#
+def loadPass(pwfile):
+    try:
+        fo = open(pwfile)
+        pw = fo.read().strip()
+        fo.close()
+        return pw
+    except Exception as e:
+        log.warn("Caught exception: %r" % e)
+        log.warn("Output: %s" % e.output)
+        return None
 
 #
 # Function to wrap the rsync call
@@ -44,17 +58,23 @@ def syncToNAS(usr,pwd,hst,src,dst):
 #
 xTime = datetime.now()
 
+# Load the password
+pw = loadPass("pushPhotos.pass")
+if pw is None:
+    log.warn("Failed to load password . . . exiting")
+    exit
+
 src='/Users/ftpuser/CameraSync/'
 log.info("Syncing: %s" % src)
 yTime = datetime.now()
-syncToNAS('admin','','10.0.0.12',src,'/volume1/PhotoSync/ftpuser/CameraSync/')
+syncToNAS('admin',pw,'10.0.0.12',src,'/volume1/PhotoSync/ftpuser/CameraSync/')
 runtime = datetime.now() - yTime
 log.info("Runtime: %s" % runtime)
 
 src='/Users/Shared/Pictures/'
 log.info("Syncing: %s" % src)
 yTime = datetime.now()
-syncToNAS('admin','','10.0.0.12',src,'/volume1/PhotoSync/Shared/Pictures/')
+syncToNAS('admin',pw,'10.0.0.12',src,'/volume1/PhotoSync/Shared/Pictures/')
 runtime = datetime.now() - yTime
 log.info("Runtime: %s" % runtime)
 
