f0c4102a50b271034f85da1336b5522d77f083ed
Eric Wall <eric@WalrusMini.local>
2016-04-17 02:28:24 -0400

    Stub

--- epdown/epdown.py
+++ epdown/epdown.py
@@ -1,3 +1,41 @@
+#!/usr/bin/python
+
 #
-# Script to download images from Reddit EarthPorn sub (just pictures of nature, I swear)
+# Imports
 #
+#import subprocess
+import logging
+#import string
+from datetime import datetime
+import sys
+import requests
+
+
+
+#
+# Setup logging
+#
+logging.basicConfig(level=logging.DEBUG,   #Default to INFO - raise to DEBUG for development and troubleshooting
+                    format='%(asctime)s %(name)-5s %(levelname)-8s %(funcName)s %(message)s',
+                    datefmt='%m-%d %H:%M',
+                    filename= str(sys.argv[0]+".log") ,
+                    filemode='w')
+log = logging.getLogger()
+
+
+
+#
+def foo():
+    log.debug("Try one")
+    response = requests.get("http://reddit.com/api/v1/me")
+    log.debug("Response: %s",response.headers)
+
+
+
+#
+# Main
+#
+start = datetime.now()
+log.info("Script start . . .")
+foo()
+log.info("Runtime: %s\n" % str(datetime.now() - start))
